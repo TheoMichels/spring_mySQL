064875d7ab20bb8c1a4c0fe235f86a894f348ee8
Théo Michels <theomichels@hotmail.fr>
2021-10-08 10:38:16 +0200

    add lombok annotation @Data to remove getters & setters

--- src/main/java/fr/polytech/serviceweb/Students/Model/Student.java
+++ src/main/java/fr/polytech/serviceweb/Students/Model/Student.java
@@ -1,10 +1,13 @@
 package fr.polytech.serviceweb.Students.Model;
 
+import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
+//allow to remove getters & setters
+@Data
 //create entity and set table name
 @Entity
 @Table(name="student")
@@ -27,36 +30,36 @@ public class Student {
     @Column(name="school_year")
     private String school_year;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getSchool_year() {
-        return school_year;
-    }
-
-    public void setSchool_year(String school_year) {
-        this.school_year = school_year;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getSchool_year() {
+//        return school_year;
+//    }
+//
+//    public void setSchool_year(String school_year) {
+//        this.school_year = school_year;
+//    }
 
 }
